d3582a0aedd6cfa62cb7d6226338bb15ed8c04b3
João Pedro <jp.dovale@hotmail.com>
2024-09-12 09:33:41 -0300

    building apk

--- src/components/LabelProduct.tsx
+++ src/components/LabelProduct.tsx
@@ -4,17 +4,19 @@ import { ButtonRemove } from "./ButtonRemove";
 
 export function LabelProduct({name, price}){
     return(
-        <HStack mb={"$4"} >
+        <HStack mb={"$4"} maxWidth={"100%"}>
             <Box 
                 bg="$red100" 
-                h={"$12"}  
+                h={"$12"}
+                w={"69%"}
                 alignItems="center" justifyContent="center"
                 rounded={"$2xl"}
                 mr={"$3"}
+                p={"$2"}
             >
-                <HStack width="74%" justifyContent="space-between" alignItems="center">
-                    <Text fontWeight={"bold"} textTransform="uppercase">{name}</Text>
-                    <Text fontWeight={"bold"}>{price}</Text>
+                <HStack  width={"$full"} justifyContent="space-between" alignItems="center">
+                    <Text fontWeight={"bold"} textTransform="uppercase" flex={1}>{name}</Text>
+                    <Text fontWeight={"bold"} >{price}</Text>
                 </HStack>
             </Box>
             <HStack space={"md"}>
